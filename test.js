a4c76a2ed2ed82755a9938e4201b488a83894e79
Emmanuel Pirsch <emmanuel@pirsch.org>
2013-01-15 20:35:56 -0500

    Add test for format strings with position.

--- test.js
+++ test.js
@@ -27,4 +27,5 @@ vows.describe('format-foo').addBatch({
   , 'I am %5d years old with left padded zeroes': withValues(123).formatTo('I am 00123 years old with left padded zeroes')
   , 'Today is %tY-%tm-%td': withValues(new Date('August 29, 2012')).formatTo('Today is 2012-08-29')
   , 'Now it is %tI:%tM%tp': withValues(new Date(2012, 8-1, 29, 22, 13)).formatTo('Now it is 10:13PM')
+  , 'Hello, my name is %1$s. Hi %1$s my name is %2$s.': withValues('Foo', 'Bar').formatTo('Hello, my name is Foo. Hi Foo my name is Bar.')
 }).export(module)
